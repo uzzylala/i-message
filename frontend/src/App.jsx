30831a0a4ddb32b442495ac98a8b68d7e018b21f
uzezi <kinguzezi@gmail.com>
2026-08-03 08:36:06 +0100

    feat(frontend): implement authentication state management and api client
    Add Zustand store for managing authentication state and socket connections.
    Integrate axios for API requests and react-hot-toast for notifications.
    
    - Add `useAuthStore` to handle user authentication and online users
    - Implement `axiosInstance` with base URL configuration
    - Integrate `socket.io-client` for real-time user presence
    - Add `react-hot-toast` for UI feedback
    - Update `App.jsx` to handle auth state synchronization via useEffect
    - Add utility functions for message formatting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,48 @@
 // import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
 import { Navigate, Route, Routes } from "react-router";
 import { useAuth } from "@clerk/react";
+import { useAuthStore } from "./store/useAuthStore";
 import ChatPage from "./pages/ChatPage";
 import AuthPage from "./pages/AuthPage";
 import PageLoader from "./components/PageLoader";
+import { useEffect } from "react";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   const { isSignedIn, isLoaded } = useAuth();
-  if (!isLoaded) {
+
+  //option 1
+  // const {clearAuth, checkAuth, isCheckingAuth} = useAuthStore();
+
+  //option 2 (better for performance)
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+  const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    if (!isSignedIn) checkAuth();
+    else clearAuth();
+  }, [checkAuth, isLoaded, isSignedIn, clearAuth]);
+
+  if (!isLoaded || (isSignedIn && isCheckingAuth)) {
     return <PageLoader />;
   }
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={isSignedIn ? <ChatPage /> : <Navigate to="/auth" replace />}
-      />
-      <Route
-        path="/auth"
-        element={!isSignedIn ? <AuthPage /> : <Navigate to="/" replace />}
-      />
-    </Routes>
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={isSignedIn ? <ChatPage /> : <Navigate to="/auth" replace />}
+        />
+        <Route
+          path="/auth"
+          element={!isSignedIn ? <AuthPage /> : <Navigate to="/" replace />}
+        />
+      </Routes>
+      <Toaster />
+    </>
   );
 }
 
